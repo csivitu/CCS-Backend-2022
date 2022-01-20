d940eae0295426b7f101b44eee3eabc7edf6f581
vidhik2002 <vidhik2002@gmail.com>
2022-01-20 15:16:33 +0530

    feat: Deserialize user

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 
 const port = config.get<number>("port");
 
 const app = express();
-
+app.use(deserializeUser);
 // const templateFolder = path.join(__dirname, "templates");
 
 app.use(express.json());
