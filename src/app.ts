c5fa00889eaa3461e2b084dcced6f8c3f9f437c3
salt57 <sourishgupta02@gmail.com>
2022-02-01 01:42:30 +0530

    feat: multithreaded nodejs :)

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,33 @@
 import express from "express";
 import config from "config";
 import cors from "cors";
-// import hbs from "express-handlebars";
-// import path from "path";
+import cluster from "cluster";
+import os from "os";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
 import deserializeUser from "./middleware/deserializeUser";
 
+const cpus = os.cpus().length;
+
 const port = config.get<number>("port");
 
 const app = express();
-// const templateFolder = path.join(__dirname, "templates");
+
 app.use(cors());
 app.use(express.json());
 app.use(deserializeUser);
-// app.set("views", path.join(__dirname, "templates"));
 
-// app.engine(
-//   "html",
-//   hbs.engine({
-//     extname: ".html",
-//     layoutsDir: templateFolder,
-//   })
-// );
-
-// app.set("view engine", "html");
-
-app.listen(port, async () => {
-  logger.info(`App is running at http://localhost:${port}`);
-  await connect();
-  routes(app);
-});
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus; i += 1) {
+    cluster.fork();
+  }
+} else {
+  app.listen(port, async () => {
+    logger.info(
+      `App is running at http://localhost:${port} with pid: ${process.pid}`
+    );
+    await connect();
+    routes(app);
+  });
+}
