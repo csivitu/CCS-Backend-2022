aad704f4c64bcb04476fa9b90702831ecc788e8b
vidhik2002 <vidhik2002@gmail.com>
2022-02-02 15:14:31 +0530

    feat: changed test duration

--- config/default.ts
+++ config/default.ts
@@ -25,7 +25,7 @@ f7nAWWlOhIzawxMKZwIDAQAB
   verify_link: process.env.VERIFY_LINK,
   reset_link: process.env.RESET_LINK,
   number_of_questions: 10,
-  test_duration: 30,
+  test_duration: 35,
   mailgun_api_key: process.env.MAILGUN_API_KEY,
   email_domain: process.env.EMAIL_DOMAIN,
   mailgun_host: process.env.MAILGUN_HOST,
