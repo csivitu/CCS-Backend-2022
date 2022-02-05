811576158e384651323101c1a9d88c1c55a45a9b
salt57 <sourishgupta02@gmail.com>
2022-02-05 13:29:29 +0530

    feat: increased rate limit

--- src/utils/limiters.ts
+++ src/utils/limiters.ts
@@ -3,14 +3,14 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: config.get("enviornment") === "production" ? 100 : 10000,
+  max: config.get("enviornment") === "production" ? 250 : 10000,
   standardHeaders: true,
   legacyHeaders: false,
 });
 
 export const createAccountLimiter = rateLimit({
   windowMs: 30 * 60 * 1000,
-  max: config.get("enviornment") === "production" ? 5 : 10000,
+  max: config.get("enviornment") === "production" ? 10 : 10000,
   message:
     "Too many accounts created from this IP, please try again after some time",
   standardHeaders: true,
@@ -19,7 +19,7 @@ export const createAccountLimiter = rateLimit({
 
 export const forgotPasswordLimiter = rateLimit({
   windowMs: 30 * 60 * 1000,
-  max: config.get("enviornment") === "production" ? 5 : 10000,
+  max: config.get("enviornment") === "production" ? 10 : 10000,
   message:
     "Too many attempts to reset password from this IP, please try again after some time",
   standardHeaders: true,
@@ -28,7 +28,7 @@ export const forgotPasswordLimiter = rateLimit({
 
 export const emailVerifyLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: config.get("enviornment") === "production" ? 3 : 10000,
+  max: config.get("enviornment") === "production" ? 10 : 10000,
   message:
     "Too many attempts to send verification mail from this IP, please try again after some time",
   standardHeaders: true,
